7e30d74f7b1a9c6c249ea79e026fad8ef1f87a9b
rodion.alukhanov <rodion.alukhanov@localhost>
2015-09-09 12:13:50 +0200

    consts file is splitted into consts and defaults.

--- src/yc-tracking.const.plentymarkets.js
+++ src/yc-tracking.const.plentymarkets.js
@@ -1,16 +1,6 @@
 // {id} will be changed with current product id
 
 var YC_CUSTOMER_ID = 906,
-    YC_BREADCRUMBS_SELECTOR = 'breadcrumbs',
-    YC_ADD_BASKET_BUTTON_SELECTOR = '[data-plenty="addBasketItemButton"]',
-    YC_ARTICLE_ID_SELECTOR = 'ArticleID',
-    YC_ARTICLE_PRICE_SELECTOR = '#price_dynamic_0_{id}',
-    YC_ARTICLE_TITLE_SELECTOR = '.itemTitle',
-    YC_ARTICLE_IMAGE_SELECTOR = '#plenty_xl_image_{id}_0',
-    YC_CATEGORY_LIST_PRODUCTS = '[name="ArticleID"]',
-    YC_PRODUCT_ATTRIBUTES = ['url', 'title', 'price', 'image'],
-    YC_RENDER_PRICE_FORMAT = '{price}&thinsp;{currencySign}',
-    YC_DECIMAL_SEPARATOR = ',',
     YC_RECO_TEMPLATES = {
         personal: {
             html_template: "<div class='col-lg-12'><h3>{{title}}</h3><div class='categoryView isGridView'>" +
