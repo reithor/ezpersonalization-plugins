ca1ec6b062db946d03bd081f8c95e8ba83e5cb74
Kostandin Dimitrijevic <kostandin.dimitrijevic@soprex.com>
2015-10-08 11:37:07 +0200

    [Plentymarkets] #7 Updated template so click on image also forwards to product details page.

--- src/yc-tracking.const.plentymarkets.js
+++ src/yc-tracking.const.plentymarkets.js
@@ -13,8 +13,8 @@ var YC_RECO_TEMPLATES = {
         "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-personal margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
         "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
         "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
-        "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
+        "<a href='{{url}}'><img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div>" +
         "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
@@ -37,8 +37,8 @@ var YC_RECO_TEMPLATES = {
         "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-bestseller margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
         "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
         "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
-        "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
+        "<a href='{{url}}'><img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div>" +
         "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
@@ -61,8 +61,8 @@ var YC_RECO_TEMPLATES = {
         "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-related margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
         "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
         "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
-        "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
+        "<a href='{{url}}'><img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div>" +
         "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
@@ -85,8 +85,8 @@ var YC_RECO_TEMPLATES = {
         "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-upselling margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
         "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
         "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
-        "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
+        "<a href='{{url}}'><img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div>" +
         "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
@@ -109,8 +109,8 @@ var YC_RECO_TEMPLATES = {
         "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-crossselling margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
         "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
         "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
-        "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
+        "<a href='{{url}}'><img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div>" +
         "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
@@ -130,12 +130,12 @@ var YC_RECO_TEMPLATES = {
     category_page: {
         html_template: "<div class='col-lg-12'><h3>{{{const.title}}}</h3><div class='categoryView isGridView'>" +
         "<ul class='row categoryDetails isGridView' data-plenty-details='categoryDetails'>{{#each rows}}{{#each columns}}" +
-        "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-category_page margin-bottom-2 center itemBox tileView onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'>" +
-        "<div class='itemBoxInner'><div id='previewImages-{{itemId}}' class='h-lg-2 h-md-3 h-sm-2 h-xs-6'>" +
-        "<div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'>" +
+        "<li class='col-xs-12 col-sm-4 col-md-3 col-lg-2 rendered-category_page margin-bottom-2 center itemBox tileView" +
+        " onHover action-2' data-plenty='item' data-plenty-id='{{itemId}}'><div class='itemBoxInner'><div id='previewImages-{{itemId}}'" +
+        " class='h-lg-2 h-md-3 h-sm-2 h-xs-6'><div class='imageBox h-lg-2 h-md-3 h-sm-2 h-xs-6 adapt-line-height'><a href='{{url}}'>" +
         "<img class='center img-responsive id-{{itemId}}' src='{{image}}' data-original='{{image}}' data-plenty-link='item-{{itemId}}'" +
-        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></div></div>" +
-        "<a class='name block' href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
+        " data-plenty-lazyload='fadeIn' style='display: inline-block;' ></a></div></div><a class='name block' " +
+        "href='{{url}}' data-plenty-href='item-{{itemId}}'>{{title}}</a><p class='price bold' data-plenty-link='item-{{itemId}}'>" +
         "<span class='large linkToItem'>{{{price}}}</span></p><p class='small' data-plenty-link='item-{{itemId}}'>&nbsp;</p>" +
         "<div class='visible-hover'><div class='basketButtonContainer clearfix'><div class='buttonBox isViewItem'>" +
         "<a class='btn btn-primary' href='{{url}}'><span class='glyphicon glyphicon-eye-open'></span>{{{../../const.details}}}</a>" +
