953bc2b3a6cef8e30476835f5c77383871f1f6de
rodio <rodio@NB-CGN2-12>
2015-11-10 11:05:23 +0100

    [Woocommerce]  V1.1

---
+++ src/plugins/shopware/Frontend/YoochooseJsTracking/Views/frontend/plugins/yoochoose_jstracking/box_article.tpl
@@ -0,0 +1,9 @@
+{extends file='parent:frontend/listing/box_article.tpl'}
+
+{block name='frontend_listing_box_article_actions_buy_now' append}
+    <input type="hidden" name="yc_articleId" value="{$sArticle.articleID}">
+{/block}
+
+{block name='frontend_listing_box_article_content' append}
+    <input type="hidden" name="yc_articleId" value="{$sArticle.articleID}">
+{/block}
---
+++ src/yc-tracking.defaults.plentymarkets.js
@@ -0,0 +1,18 @@
+
+// {id} will be changed with current product id
+var YC_BREADCRUMBS_SELECTOR = '.breadcrumbs a',
+    YC_ADD_BASKET_BUTTON_SELECTOR = '[data-plenty="addBasketItemButton"]',
+    YC_ARTICLE_BASKET_SELECTOR = '.toBasketWrapper a',
+    YC_ARTICLE_ID_SELECTOR = '[name="ArticleID"]',
+    YC_ARTICLE_ID_VALUE = 'value',
+    YC_ARTICLE_PRICE_SELECTOR = '#price_dynamic_0_{id}',
+    YC_ARTICLE_PRICE_VALUE = 'innerHTML',
+    YC_ARTICLE_TITLE_SELECTOR = '.itemTitle',
+    YC_ARTICLE_TITLE_VALUE = 'innerHTML',
+    YC_ARTICLE_IMAGE_SELECTOR = '#plenty_xl_image_{id}_0',
+    YC_ARTICLE_IMAGE_VALUE = 'src',
+    YC_CATEGORY_LIST_PRODUCTS = '[name="ArticleID"]',
+    YC_RENDER_PRICE_FORMAT = '{price}&thinsp;{currencySign}',
+    YC_DECIMAL_SEPARATOR = ',',
+    YC_SEARCH_SELECTED_SELECTOR = ' .yc-hover', //it has prefix of the result tag id e.g ('#ycSearchResult0' + YC_SEARCH_SELECTED_SELECTOR)
+    YC_SEARCH_ALL_RESULTS_SELECTOR = ' .yc-search-result-item'; //it has prefix of the result tag id e.g ('#ycSearchResult0' + YC_SEARCH_ALL_RESULTS_SELECTOR)
