02c3b1faa992b9728176f57815fd59d902de4626
Filip Petrovic <Filip Petrovic@localhost>
2016-07-15 12:20:16 +0200

    Send Magento 1 credentials automatically.(Sends appKey and appSecret)

--- src/plugins/magento/app/code/community/Yoochoose/JsTracking/Model/Observer.php
+++ src/plugins/magento/app/code/community/Yoochoose/JsTracking/Model/Observer.php
@@ -1,6 +1,6 @@
 <?php
 
-class Yoochoose_JsTracking_Model_Observer 
+class Yoochoose_JsTracking_Model_Observer
 {
     const YOOCHOOSE_LICENSE_URL = 'https://admin.yoochoose.net/api/v4/';
 
@@ -13,6 +13,7 @@ class Yoochoose_JsTracking_Model_Observer
      */
     public function trackBuy($observer)
     {
+        Mage::getStoreConfig('yoochoose/general/endpoint');
         $orderIds = $observer->getEvent()->getOrderIds();
         if (empty($orderIds) || !is_array($orderIds)) {
             return;
@@ -32,6 +33,7 @@ class Yoochoose_JsTracking_Model_Observer
     {
         $customerId = Mage::getStoreConfig('yoochoose/general/customer_id');
         $licenseKey = Mage::getStoreConfig('yoochoose/general/license_key');
+        $apiCredentials = array('consumerKey' => '', 'consumerSecret' => '');
         $code = Mage::getSingleton('adminhtml/config_data')->getStore();
         $scopeId = Mage::getModel('core/store')->load($code)->getId();
         if ($scopeId === null) {
@@ -43,13 +45,22 @@ class Yoochoose_JsTracking_Model_Observer
             return;
         }
 
+        if($this->isApiClientConfigured()) {
+            $apiCredentials = $this->getApiCredentialsFromDatabase();
+        }else{
+            Mage::getSingleton('adminhtml/session')->addWarning('Please, go to Data Import Configuration tab and configure new api client, to allow communication between your store and Yoochoose!');
+        }
+
+
         try {
             $body = array(
                 'base' => array(
                     'type' => 'MAGENTO',
                     'pluginId' => Mage::getStoreConfig('yoochoose/general/plugin_id'),
                     'endpoint' => Mage::getStoreConfig('yoochoose/general/endpoint'),
-                    ), 
+                    'appKey' => $apiCredentials['consumerKey'],
+                    'appSecret' => $apiCredentials['consumerSecret'],
+                ),
                 'frontend' => array(
                     'design' => Mage::getStoreConfig('yoochoose/general/design'),
                 ));
@@ -64,7 +75,6 @@ class Yoochoose_JsTracking_Model_Observer
         } catch (Exception $ex) {
             Mage::throwException('Plugin registration failed: ' . $ex->getMessage());
         }
-
     }
 
     /**
@@ -80,11 +90,38 @@ class Yoochoose_JsTracking_Model_Observer
             $productModel = Mage::getModel('catalog/product');
             $attr = $productModel->getResource()->getAttribute('manufacturer');
             if ($attr->usesSource()) {
-                 $manufacturerId = $attr->getSource()->getOptionId($manufacturerName);
+                $manufacturerId = $attr->getSource()->getOptionId($manufacturerName);
             }
 
             $collection = $block->getLoadedProductCollection();
             $collection->addAttributeToFilter('manufacturer', $manufacturerId);
         }
+    }
+
+    /**
+     * Helper function to check if yoochoose api client is configured.
+     *
+     * @return bool
+     */
+    private function isApiClientConfigured(){
+        $postData = Mage::app()->getRequest()->getPost();
+
+        return($postData['groups']['data_export']['fields']['consumer_key']['value'] !== null) ? true :false;
+    }
+
+    /**
+     * Prepares api credentials for sending to Yoochoose.
+     *
+     * @return array
+     */
+    private function getApiCredentialsFromDatabase(){
+        $oauthConsumer = Mage::getModel('oauth/consumer')->getCollection()
+            ->addFieldToFilter('name', 'Yoochoose-Consumer')
+            ->getFirstItem();
+
+        $apiCredentials['consumerKey'] = $oauthConsumer->getKey();
+        $apiCredentials['consumerSecret'] = $oauthConsumer->getSecret();
+
+        return $apiCredentials;
     }
 }
