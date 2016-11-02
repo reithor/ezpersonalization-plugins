9ab139aa188af2b7cd2998a01636b3946eb3f85f
Vojin Janevski <Vojin Janevski@localhost>
2016-11-02 11:37:00 +0100

    LCOM-26: Magento 1 export to files implementation in progress.

--- src/plugins/magento/app/code/community/Yoochoose/JsTracking/Helper/Data.php
+++ src/plugins/magento/app/code/community/Yoochoose/JsTracking/Helper/Data.php
@@ -1,7 +1,13 @@
 <?php
 
+
 class Yoochoose_JsTracking_Helper_Data extends Mage_Core_Helper_Abstract
 {
+
+    const YC_MAX_FILE_SIZE = 52428800; // max file size size in bytes 50Mb
+
+    const YC_DIRECTORY_NAME = 'yoochoose';
+
 
     public function _getHttpPage($url, $body, $customerId, $licenceKey)
     {
@@ -55,6 +61,111 @@ class Yoochoose_JsTracking_Helper_Data extends Mage_Core_Helper_Abstract
         curl_close($cURL);
 
         return $result;
+    }
+
+    /**
+     * export to files
+     *
+     * @param int $limit
+     * @return string postData
+     */
+    public function export($limit)
+    {
+        $ycResponseFormats = [
+            'MAGENTO2', 'MAGENTO2_CATEGORIES', 'MAGENTO2_VENDORS'
+        ];
+
+        $postData = [
+            'transaction' => null,
+            'events' => []
+        ];
+
+        foreach ($ycResponseFormats as $format){
+            $postData['events'][] = [
+                'action' => 'FULL',
+                'format' => $format,
+                'contentType' => '1',
+                'credentials' => [
+                    'login' => null,
+                    'password' => null
+                ],
+                'uri' => []
+            ];
+        }
+
+        $directory = Mage::getBaseDir('media') . '/' . self::YC_DIRECTORY_NAME . '/';
+
+        $file = new Varien_Io_File();
+
+        $file->rmdirRecursive($directory);
+        $file->mkdir($directory, 0775);
+
+        $postData = $this->exportData('Products', $postData, $directory, $limit, 0);
+        $postData = $this->exportData('Categories', $postData, $directory, $limit, 1);
+        $postData = $this->exportData('Vendors', $postData, $directory, $limit, 2);
+
+        return $postData;
+
+    }
+
+    /**
+     * Generates random string with $length characters
+     *
+     * @param int $length
+     * @return string
+     */
+    private function generateRandomString($length = 20)
+    {
+        $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+        $charactersLength = strlen($characters);
+        $randomString = '';
+        for ($i = 0; $i < $length; $i++) {
+            $randomString .= $characters[rand(0, $charactersLength - 1)];
+        }
+
+        return $randomString;
+    }
+
+    /**
+     * Exports data to file and returns $postData parameter
+     *   with URLs to files
+     *
+     * @param string $method
+     * @param array $postData
+     * @param string $directory
+     * @param integer $limit
+     * @param integer $exportIndex
+     * @return array $postData
+     */
+    private function exportData($method, $postData, $directory, $limit = 0, $exportIndex = 0){
+
+        $io = new Varien_Io_File();
+        $baseUrl =  Mage::getBaseUrl('media');
+        $fileUrl = $baseUrl . self::YC_DIRECTORY_NAME . '/';
+
+        $method = 'get' . $method;
+        Mage::app()->getRequest()->setParam('limit', $limit);
+        $offset = 0;
+
+        do {
+            Mage::app()->getRequest()->setParam('offset', $offset);
+            $results = Mage::getModel('yoochoose_jstracking/YoochooseExport')->$method();
+            if (!empty($results)){
+                $filename = $this->generateRandomString();
+                $file = $directory . $filename;
+                $io->write($file, json_encode($results));
+                $fileSize = filesize($file);
+                if ($fileSize >= self::YC_MAX_FILE_SIZE){
+                    $io->rm($file);
+                    $this->getRequest()->setPost('limit', --$limit);
+                } else {
+                    $postData['events'][$exportIndex]['uri'][] = $fileUrl . $filename;
+                    $offset = $offset + $limit;
+                }
+            }
+        } while (!empty($results));
+
+        return $postData;
     }
 
 }
