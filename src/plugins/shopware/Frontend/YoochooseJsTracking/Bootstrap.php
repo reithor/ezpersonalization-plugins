e7aeb87a5bac407a85fdba73146b039aac24f87a
joao.dinis <joao.dinis@localhost>
2018-03-26 14:26:59 +0200

    LCOM-246: updated shopware plugin version to 2.5.2.

--- src/plugins/shopware/Frontend/YoochooseJsTracking/Bootstrap.php
+++ src/plugins/shopware/Frontend/YoochooseJsTracking/Bootstrap.php
@@ -96,7 +96,7 @@ class Shopware_Plugins_Frontend_YoochooseJsTracking_Bootstrap extends Shopware_C
      */
     public function getVersion()
     {
-        return '2.5.1';
+        return '2.5.2';
     }
 
     /**
